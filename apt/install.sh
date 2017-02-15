1432eb157f4589fe0959444603ec518436462469
Tobias Schulte <Tobias.Schulte@kratzer-automation.com>
2017-02-15 15:26:44 +0100

    don't install homebrew on ubuntu based systems, use apt instead

---
+++ apt/install.sh
@@ -0,0 +1,39 @@
+# If we are on an ubuntu based system, update all installed packages.
+
+if test $(which apt)
+then
+  echo "› adding repositories to apt"
+  sudo apt-add-repository -y ppa:webupd8team/java
+  sudo apt-add-repository -y ppa:webupd8team/atom
+  sudo add-apt-repository -y ppa:mmk2410/intellij-idea
+  sudo add-apt-repository -y ppa:benoit.pierre/plover
+
+  echo "› sudo apt update && apt upgrade"
+  sudo apt update && apt upgrade
+
+  echo "› installing required software"
+  sudo apt install \
+    atom \
+    autofs \
+    bash-completion \
+    dos2unix \
+    fonts-inconsolata \
+    git \
+    git-gui \
+    gitk \
+    graphviz \
+    intellij-idea-community \
+    myrepos \
+    oracle-java7-installer \
+    oracle-java7-unlimited-jce-policy \
+    oracle-java8-installer \
+    oracle-java8-unlimited-jce-policy \
+    oracle-java9-installer \
+    oracle-java9-unlimited-jce-policy \
+    vim \
+    vnc4server \
+    wireshark-qt\
+    xclip \
+    zsh \
+
+fi
