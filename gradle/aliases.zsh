ec5ecc7f14add02128a8be0ef661a7d1676cc9e4
Tobias Schulte <Tobias.Schulte@kratzer-automation.com>
2017-02-16 10:48:04 +0100

    gradle and java

---
+++ gradle/aliases.zsh
@@ -0,0 +1,3 @@
+alias gw='DISPLAY=:1 $(upfind -name gradlew)'
+alias g="DISPLAY=:1 gradle"
+alias gwr='$(upfind -name gradlew) :run'
---
+++ java/java-opts.zsh
@@ -0,0 +1,2 @@
+# workaround for http://bugs.java.com/view_bug.do?bug_id=6714678
+export JAVA_OPTS=-Dsun.awt.disablegrab=true
