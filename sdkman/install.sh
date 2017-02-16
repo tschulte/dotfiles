554158ba9c8bf43da36e2c97013e49fabac3d550
Tobias Schulte <Tobias.Schulte@kratzer-automation.com>
2017-02-16 10:25:53 +0100

    use sdkman

---
+++ sdkman/install.sh
@@ -0,0 +1,4 @@
+if test ! -s ~/.sdkman/bin/sdkman-init.sh
+then
+  curl -s "https://get.sdkman.io" | bash
+fi
---
+++ sdkman/path.zsh
@@ -0,0 +1,5 @@
+if test -s ~/.sdkman/bin/sdkman-init.sh
+then
+  export SDKMAN_DIR=~/.sdkman
+  source ~/.sdkman/bin/sdkman-init.sh
+fi
