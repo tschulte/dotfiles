e10053c698f97b26e17f33ac2ec15e65438a44bb
Tobias Schulte <Tobias.Schulte@kratzer-automation.com>
2017-03-09 09:44:12 +0100

    fix test when no brew installed

--- git/completion.zsh
+++ git/completion.zsh
@@ -2,8 +2,7 @@
 # bash `git-completion` script at $completion below (this is where Homebrew
 # tosses it, at least).
 completion='$(brew --prefix)/share/zsh/site-functions/_git'
-
-if test -f $completion
+if test -f "$completion"
 then
   source $completion
 fi
